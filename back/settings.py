8cc193f1263de9dfa731e1cec7be418e7fbba400
Ali <aliali.ali1378@yahoo.com>
2022-05-29 22:18:23 +0430

    Add Url to Allowed Cors

--- back/settings.py
+++ back/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:8081",
+    "http://alimahdavimehr.ir",
     "https://alimahdavimehr.ir",
-    "https://alimahdavimehr.ir:8080",
 
     "http://127.0.0.1:8080",
 ]
